084c89df730bee46d4e67dd945c7459851431b80
neominalsolutions <mert.alptekin@neominal.com>
2022-03-30 18:42:16 +0300

    Multiple Subscriber Test

--- NeominalRabbitMq.Publisher/Program.cs
+++ NeominalRabbitMq.Publisher/Program.cs
@@ -7,6 +7,8 @@ using System.Text;
 var factory = new ConnectionFactory();
 factory.Uri = new Uri("amqps://qosgqhkq:b-jNboVWOOVgxSzmWKL36oCE7qLX51vg@sparrow.rmq.cloudamqp.com/qosgqhkq");
 
+// publisher mesajı gönderdiğinde subscriberlar tarafından doğru işlendiğinde ilgili mesajı silme işlemi yapıp. Daha güvenli bir şekilde mesajları işleyebiliriz.
+// Kuyrukta 10 adet mesaj var tek bir seferde kaçar kaçar mesaj göndereceğiz kısmını halledeceğiz. Tek seferde 10'ar mesaj alsın diyebiliriz. Her subscriber tek seferde 10 mesaj gönder dilebiliriz. Mesaj işlenme süresi uzun ise her subscriber 1 yada 2 tane gönderilmesi uygun olacaktır.
 
 using (var cnn = factory.CreateConnection()) // connection açtık
 {
@@ -15,11 +17,26 @@ using (var cnn = factory.CreateConnection()) // connection açtık
     // exclusive burada oluşan kanal üzerinden sadece erişebilirim fakat subscriber farklı kanaldan bağlantı kurulacaktır.
     // işlem bitince kuyruğu otomatik silmesin. en son kalan subscriber da işini bittikten sonra kuyruğu siler.
     channel.QueueDeclare("test-queue",durable:true, exclusive:false,autoDelete:false);
-    string message = "Hello Word"; // byte[] olarak verileri gönderebiliryoruz bu sebeple istediğimiz herşeyi gönderebiliriz.
-    var messageBody = Encoding.UTF8.GetBytes(message);
-    channel.BasicPublish(string.Empty, "test-queue", null, messageBody); // exchange kullanmadığımız işleme default exchange denir.
+   
+    
 
-    Console.WriteLine("Mesaj gönderildi");
+
+    Enumerable.Range(1, 10).ToList().ForEach(x =>
+     {
+         string message = $"Test-{x}"; // byte[] olarak verileri gönderebiliryoruz bu sebeple istediğimiz herşeyi gönderebiliriz.
+         var messageBody = Encoding.UTF8.GetBytes(message);
+         
+
+         Thread.Sleep(1000);
+         channel.BasicPublish(string.Empty, "test-queue", null, messageBody); // exchange kullanmadığımız işleme default exchange denir.
+
+
+         Console.WriteLine($"Mesaj gönderildi {x}");
+
+     });
+
+
+
     Console.ReadKey();
 
 }
--- NeominalRabbitMq.Subscriber/Program.cs
+++ NeominalRabbitMq.Subscriber/Program.cs
@@ -5,32 +5,46 @@ using RabbitMQ.Client;
 using RabbitMQ.Client.Events;
 using System.Text;
 
-var factory = new ConnectionFactory();
-factory.Uri = new Uri("amqps://qosgqhkq:b-jNboVWOOVgxSzmWKL36oCE7qLX51vg@sparrow.rmq.cloudamqp.com/qosgqhkq");
 
 
-using (var cnn = factory.CreateConnection()) // connection açtık
-{
+
+    var factory = new ConnectionFactory();
+    factory.Uri = new Uri("amqps://qosgqhkq:b-jNboVWOOVgxSzmWKL36oCE7qLX51vg@sparrow.rmq.cloudamqp.com/qosgqhkq");
+
+
+    using var cnn = factory.CreateConnection(); // connection açtık
+
     var channel = cnn.CreateModel(); // kanal açtık
     // durable önemli fiziki olarak mesaj kaybolmasın persistant yaptık. false in-memory
     // exclusive burada oluşan kanal üzerinden sadece erişebilirim fakat subscriber farklı kanaldan bağlantı kurulacaktır.
     // işlem bitince kuyruğu otomatik silmesin. en son kalan subscriber da işini bittikten sonra kuyruğu siler.
-    channel.QueueDeclare("test-queue", durable: true, exclusive: false, autoDelete: false); // kuyruk tanımı
+    // channel.QueueDeclare("test-queue", durable: true, exclusive: false, autoDelete: false); // kuyruk tanımı
     // eğer publisher bu kuruğu oluşturmuş ise bundan eminsek bunu tekrar declare etmeye gerek yok. fakat publisher bu kuyruğu oluşturmamış ise o zaman subscriber bu kuyruğu oluşturur. channel.QueueDeclare için aynı parametrelerin olmasına dikkat edelim.
 
+    channel.BasicQos(0,2 ,false); // global değeri true yaparsak kaç tane subscriber var tek bir seferde tüm subscriberlara 5 adet dağıtacak şekilde global olarak ayarlar. her bir subcriber için kaç adet gönderileceğini false diyerek belirttik.
+
     var consumer = new EventingBasicConsumer(channel); // bu kanal üzerinden consumer oluşturduk.
-    channel.BasicConsume("test-queue", autoAck: true, consumer); // autoAct false yaparsak mesajı kuyruktan silmek için biz bir işlem yapacağız.                                                     
+
+    channel.BasicConsume("test-queue",false, consumer); // autoAct false yaparsak mesajı kuyruktan silmek için biz bir işlem yapacağız.                                                     
 
     // mesaj iletildiğinde çalışacak olan event.
     consumer.Received += (object? sender, BasicDeliverEventArgs e) =>
     {
+       
         var message = Encoding.UTF8.GetString(e.Body.ToArray()); // byte[] olan mesajı oku
         Console.WriteLine("Gelen Mesaj :" + message);
+        Thread.Sleep(1000);
+
+        channel.BasicAck(e.DeliveryTag, false); // kendimiz tek tek sileriz. rabbitmq haberdar ederiz.
+        // Direkt olarak kuyruğa gönderirsek bu default exchange denk gelir.
+
+
+        //channel.BasicAck(e.DeliveryTag,multiple:false); // multiple true dersek o anda ramde işlemiş mesajlarıda rabbitmq bildirir. false dersek tek tek iletildiğinde o zaman rabbitmq haberdar edilir ve silinmiş olur.  
     };
- 
 
-}
+Console.ReadLine();
 
 
-Console.WriteLine("Hello, World!");
-Console.ReadKey();
+
+//Console.WriteLine("Hello, World!");
+//Console.ReadKey();
